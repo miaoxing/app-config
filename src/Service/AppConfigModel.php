8db769e474872b387f1383f03eb7204046f46d75
twinh <twinhuang@qq.com>
2018-01-09 14:44:17 +0800

    configModel改造为appConfigModel模式

--- src/Service/AppConfigModel.php
+++ src/Service/AppConfigModel.php
@@ -10,79 +10,10 @@ use Miaoxing\Plugin\Model\SoftDeleteTrait;
 
 /**
  * 应用配置
- *
- * @property mixed $phpValue
  */
-class AppConfigModel extends BaseModelV2
+class AppConfigModel extends ConfigModel
 {
     use HasAppIdTrait;
     use SoftDeleteTrait;
     use AppConfigTrait;
-
-    protected $defaultCasts = [
-        'value' => 'mixed',
-    ];
-
-    protected $decoder = 'unserialize';
-
-    protected $encoder = 'serialize';
-
-    protected $virtual = [
-        'type_label',
-    ];
-
-    protected function getTypeLabelAttribute()
-    {
-        return wei()->configModel->getConstantLabel('type', $this['type']);
-    }
-
-    protected function getValueAttribute()
-    {
-        $value = $this->getPhpValue();
-
-        if (is_scalar($value) || $value === null) {
-            return $value;
-        }
-
-        return json_encode($value, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    }
-
-    protected function setValueAttribute($value)
-    {
-        $value = $this->covert($value, $this->get('type'));
-        $this->data['value'] = call_user_func($this->encoder, $value);
-    }
-
-    public function getPhpValue()
-    {
-        return $this->data['value'] ? call_user_func($this->decoder, $this->data['value']) : null;
-    }
-
-    /**
-     * @param string $value
-     * @param int $type
-     * @return mixed
-     */
-    protected function covert($value, $type)
-    {
-        switch ($type) {
-            case ConfigModel::TYPE_STRING:
-                return (string) $value;
-
-            case ConfigModel::TYPE_INT:
-                return (int) $value;
-
-            case ConfigModel::TYPE_FLOAT:
-                return (float) $value;
-
-            case ConfigModel::TYPE_BOOL:
-                return (bool) $value;
-
-            case ConfigModel::TYPE_ARRAY:
-                return json_decode($value, true);
-
-            default:
-                return $value;
-        }
-    }
 }
